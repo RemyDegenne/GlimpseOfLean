c3ec8f81ecea2fa8d8578fcf4e50441b4d95bd83
Remy Degenne <remydegenne@gmail.com>
2025-09-30 16:23:18 +0200

    small changes

--- GlimpseOfLean/Solutions/Topics/PDE.lean
+++ GlimpseOfLean/Solutions/Topics/PDE.lean
@@ -24,69 +24,92 @@ namespace Tutorial
 #check ftaylorSeriesWithin
 #check ContDiffOn
 
+/-- `ℝ[n]` is notation for `EuclideanSpace ℝ (Fin n)` -/
 macro "ℝ[" n:term "]" : term => `(EuclideanSpace ℝ (Fin $n))
+/-- `∂[i] f` is notation for the partial derivative of `f` with respect to the `i`-th variable -/
 macro "∂[" i:term "]" : term => `(fun f x => fderiv ℝ f x (EuclideanSpace.single $i (1:ℝ)))
+/-- `∇ f` is notation for the gradient of `f` -/
 prefix:max " ∇ " => gradient
+/-- `𝕕 f` is notation for the derivative of `f` -/
 prefix:max " 𝕕 " => deriv
 
 section Definitions
 
 variable {𝕜 E G : Type*} [NontriviallyNormedField 𝕜] [NormedAddCommGroup E] [NormedAddCommGroup G]
-  [NormedSpace 𝕜 E] [NormedSpace 𝕜 G] {U : Set E} {f : E → G} {x : E} {n : ℕ∞}
+  [NormedSpace 𝕜 E] [NormedSpace 𝕜 G] {U : Set E} {u : E → G} {x : E} {n : ℕ∞}
   {p : E → FormalMultilinearSeries 𝕜 E G} -- ∀ n : ℕ, E[×n]→L[𝕜] G
   {F : FormalMultilinearSeries 𝕜 E G → E → G}
 
-/-- A PDE has order at most `n` if its value on a function `f` at a point `x` depends only on
-the derivatives of `f` at `x` up to order `n`. -/
+/-- A PDE has order at most `n` if its value on a function `u` at a point `x` depends only on
+the derivatives of `u` at `x` up to order `n`. -/
 structure IsOrder (n : ℕ) (F : FormalMultilinearSeries 𝕜 E G → E → G) : Prop where
   congr {p q : FormalMultilinearSeries 𝕜 E G} (hpq : ∀ m ≤ n, p m = q m) : F p = F q
 
 structure IsClassicalSolution (F : FormalMultilinearSeries 𝕜 E G → E → G) (n : ℕ∞) (U : Set E)
-    (f : E → G) : Prop where
-  condDiffOn : ContDiffOn 𝕜 n f U
-  eq_zero : ∀ x ∈ U, F (ftaylorSeriesWithin 𝕜 f U x) x = 0
+    (u : E → G) : Prop where
+  condDiffOn : ContDiffOn 𝕜 n u U
+  eq_zero : ∀ x ∈ U, F (ftaylorSeriesWithin 𝕜 u U x) x = 0
 
 end Definitions
 
 section Real
 
-variable {n : ℕ}
-  {U : Set ℝ[n]} {f : ℝ[n] → ℝ} {x : ℝ[n]}
-  {p : ℝ[n] → FormalMultilinearSeries ℝ ℝ[n] ℝ}
-  {F : FormalMultilinearSeries ℝ ℝ[n] ℝ → ℝ[n] → ℝ}
+variable {n : ℕ} {U : Set ℝ[n]} {u f g : ℝ[n] → ℝ} {x : ℝ[n]}
+  {p : ℝ[n] → FormalMultilinearSeries ℝ ℝ[n] ℝ} {F : FormalMultilinearSeries ℝ ℝ[n] ℝ → ℝ[n] → ℝ}
 
-local notation "e(" n ") " => stdOrthonormalBasis ℝ ℝ[n]
+local notation "e" => stdOrthonormalBasis ℝ _
 
 #check finrank_euclideanSpace_fin
 
-def laplaceEquation (n : ℕ) : FormalMultilinearSeries ℝ ℝ[n] ℝ → ℝ[n] → ℝ := fun p _ ↦
-  ∑ i : Fin (finrank ℝ ℝ[n]), p 2 ![e(n) i, e(n) i]
+def laplaceEquation (n : ℕ) : FormalMultilinearSeries ℝ ℝ[n] ℝ → ℝ[n] → ℝ :=
+  fun p _ ↦ ∑ i : Fin (finrank ℝ ℝ[n]), p 2 ![e i, e i]
 
 #check laplacian
 
-lemma isOrder_laplaceEquation : IsOrder 2 (laplaceEquation n) where
+lemma isOrder_two_laplaceEquation : IsOrder 2 (laplaceEquation n) where
   congr {p q} hpq := by unfold laplaceEquation; rw [hpq 2 le_rfl]
 
-lemma ContDiffOn.isClassicalSolution_laplaceEquation_iff (hU : IsOpen U) (hf : ContDiffOn ℝ 2 f U) :
-    IsClassicalSolution (laplaceEquation n) 2 U f ↔ ∀ x ∈ U, Δ f x = 0 := by
+lemma ContDiffOn.isClassicalSolution_laplaceEquation_iff (hU : IsOpen U) (hu : ContDiffOn ℝ 2 u U) :
+    IsClassicalSolution (laplaceEquation n) 2 U u ↔ ∀ x ∈ U, Δ u x = 0 := by
   refine ⟨fun h x hx ↦ ?_, fun h ↦ ?_⟩
   · have h_eq := h.eq_zero x hx
     simp only [laplaceEquation, ftaylorSeriesWithin] at h_eq
     rw [laplacian_eq_iteratedFDeriv_stdOrthonormalBasis]
-    suffices iteratedFDerivWithin ℝ 2 f U x = iteratedFDeriv ℝ 2 f x by simpa [this] using h_eq
+    suffices iteratedFDerivWithin ℝ 2 u U x = iteratedFDeriv ℝ 2 u x by simpa [this] using h_eq
     rw [iteratedFDerivWithin_eq_iteratedFDeriv hU.uniqueDiffOn _ hx]
     exact h.condDiffOn.contDiffAt (hU.mem_nhds hx)
-  · refine ⟨hf, fun x hx ↦ ?_⟩
+  · refine ⟨hu, fun x hx ↦ ?_⟩
     simp only [laplaceEquation, ftaylorSeriesWithin]
     rw [laplacian_eq_iteratedFDeriv_stdOrthonormalBasis] at h
-    suffices iteratedFDerivWithin ℝ 2 f U x = iteratedFDeriv ℝ 2 f x by simpa [this] using h x hx
-    rw [iteratedFDerivWithin_eq_iteratedFDeriv hU.uniqueDiffOn (hf.contDiffAt (hU.mem_nhds hx)) hx]
+    suffices iteratedFDerivWithin ℝ 2 u U x = iteratedFDeriv ℝ 2 u x by simpa [this] using h x hx
+    rw [iteratedFDerivWithin_eq_iteratedFDeriv hU.uniqueDiffOn (hu.contDiffAt (hU.mem_nhds hx)) hx]
 
 lemma isClassicalSolution_laplaceEquation_iff (hU : IsOpen U) :
-    IsClassicalSolution (laplaceEquation n) 2 U f ↔ ContDiffOn ℝ 2 f U ∧ ∀ x ∈ U, Δ f x = 0 := by
-  refine ⟨fun h ↦ ⟨h.condDiffOn, ?_⟩, fun ⟨hf, hΔ⟩ ↦ ?_⟩
+    IsClassicalSolution (laplaceEquation n) 2 U u ↔ ContDiffOn ℝ 2 u U ∧ ∀ x ∈ U, Δ u x = 0 := by
+  refine ⟨fun h ↦ ⟨h.condDiffOn, ?_⟩, fun ⟨hu, hΔ⟩ ↦ ?_⟩
   · rwa [← ContDiffOn.isClassicalSolution_laplaceEquation_iff hU h.condDiffOn]
-  · rwa [ContDiffOn.isClassicalSolution_laplaceEquation_iff hU hf]
+  · rwa [ContDiffOn.isClassicalSolution_laplaceEquation_iff hU hu]
+
+def poissonEquation (n : ℕ) (f : ℝ[n] → ℝ) : FormalMultilinearSeries ℝ ℝ[n] ℝ → ℝ[n] → ℝ :=
+  fun p x ↦ - ∑ i : Fin (finrank ℝ ℝ[n]), p 2 ![e i, e i] - f x
+
+lemma ContDiffOn.isClassicalSolution_poissonEquation_iff (hU : IsOpen U) (hu : ContDiffOn ℝ 2 u U) :
+    IsClassicalSolution (poissonEquation n f) 2 U u ↔ ∀ x ∈ U, - Δ u x = f x := by
+  refine ⟨fun h x hx ↦ ?_, fun h ↦ ?_⟩
+  · have h_eq := h.eq_zero x hx
+    simp only [poissonEquation, ftaylorSeriesWithin] at h_eq
+    rw [laplacian_eq_iteratedFDeriv_stdOrthonormalBasis]
+    simp only
+    rw [← sub_eq_zero]
+    suffices iteratedFDerivWithin ℝ 2 u U x = iteratedFDeriv ℝ 2 u x by simpa [this] using h_eq
+    rw [iteratedFDerivWithin_eq_iteratedFDeriv hU.uniqueDiffOn _ hx]
+    exact h.condDiffOn.contDiffAt (hU.mem_nhds hx)
+  · refine ⟨hu, fun x hx ↦ ?_⟩
+    simp only [poissonEquation, ftaylorSeriesWithin]
+    rw [laplacian_eq_iteratedFDeriv_stdOrthonormalBasis] at h
+    rw [sub_eq_zero]
+    suffices iteratedFDerivWithin ℝ 2 u U x = iteratedFDeriv ℝ 2 u x by simpa [this] using h x hx
+    rw [iteratedFDerivWithin_eq_iteratedFDeriv hU.uniqueDiffOn (hu.contDiffAt (hU.mem_nhds hx)) hx]
 
 end Real
 
